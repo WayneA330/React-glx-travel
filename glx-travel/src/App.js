06589d119f15cb7390c91e70e89ea3cfc3daa173
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-05-29 17:42:17 +0400

    home and pricing page done

---
+++ glx-travel/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// Different pages
+import Home from './routes/Home'
+import Pricing from './routes/Pricing'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pricing' element={<Pricing />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
